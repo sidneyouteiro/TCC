c12c701496dfe360c5e4983d294349a2c36f5e94
Sidney Outeiro <sid@dcc.ufrj.br>
2023-10-16 18:49:29 -0300

    dataframe handler added; convergence on gradient added

--- modules/classNCA.py
+++ modules/classNCA.py
@@ -1,14 +1,25 @@
+import time
+import numpy as np
+import pandas as pd
+
+from scipy.spatial.distance import euclidean
+from sklearn.preprocessing import LabelEncoder
+
 class NCA:
-    def __init__(self, optimization_func=None, max_iter=50, k=5):
+    def __init__(self, optimization_func=None, max_iter=50, k=5, cpu_count=None):
         self.optimization_func = optimization_func
         self.max_iter = max_iter
         self.k = k
+        self.cpu_count = cpu_count
 
     def fit(self, X, y):
-        from sklearn.preprocessing import LabelEncoder
-        import numpy as np
-        import time
-
+        if isinstance(X, pd.core.frame.DataFrame):
+            self.accepted_columns = X.columns.copy()
+            X = np.asarray(X)
+        if isinstance(y, pd.core.frame.DataFrame):
+            self.accepted_columns = self.accepted_columns + y.columns.copy() if hasattr(self,'accepted_columns') else y.columns.copy() 
+            y = np.asarray(y)
+            
         y = LabelEncoder().fit_transform(y)            
         t_train = time.time()
         # Compute a mask that stays fixed during optimization:
@@ -18,9 +29,17 @@ class NCA:
         # Initialize the transformation
         transformation = np.ravel(np.eye(X.shape[1],X.shape[1]))
 
-        
+        params = {
+            'X':X,
+            'same_class_mask':same_class_mask,
+            'transformation': transformation,
+            'max_iter': self.max_iter,
+            'cpu_count': self.cpu_count
+        }
+
+
         # Call the optimizer
-        opt_result = self.optimization_func(X,same_class_mask,transformation,max_iter=self.max_iter)
+        opt_result = self.optimization_func(**params)
         
            
         # Reshape the solution found by the optimizer
@@ -36,24 +55,32 @@ class NCA:
         return self
 
     def transform(self, X):
-        import numpy as np
-        
         return np.dot(X, self.components_.T)
 
     def _classify_test_object(self,test_object):
-        from scipy.spatial.distance import euclidean
-        import numpy as np
-        
         k = self.k
         class_probabilities = {i:0 for i in np.unique(self.y_train)}
         distances = np.array([euclidean(test_object, i) for i in self.X_train_transformed])
         top_k_closest_distances_indexs = np.argpartition(distances,k)[:k]
         sum_k_distances = np.sum(distances[top_k_closest_distances_indexs])
+        # usar o softmax
         for neighbor_index in top_k_closest_distances_indexs:
             class_probabilities[self.y_train[neighbor_index]] += distances[neighbor_index] / sum_k_distances
         return max(class_probabilities, key=class_probabilities.get)
     
     def predict(self, X_test):
-        X_test_transformed = self.transform(X_test)
-        
-        return [self._classify_test_object(i) for i in X_test_transformed]
+        try:
+            if type(X_test) == type(np.array([])):
+                X_test_transformed = self.transform(X_test)
+                return np.array([self._classify_test_object(i) for i in X_test_transformed])
+            elif (type(X_test) == type(pd.DataFrame([]))) and (X_test.columns == self.accepted_columns):
+                print('b')
+                X_test = np.asarray(X_test)
+                X_test_transformed = self.transform(X_test)
+                y_predict = [self._classify_test_object(i) for i in X_test_transformed]
+                return pd.DataFrame(y_predict,columns=self.accepted_columns[-1:])
+            else:
+                raise RuntimeError('[Error] foi encontrado um problema ao lidar com {X_test}')
+            
+        except Exception as err:
+            print(f'foi encontrado o seguinte error: {repr(err)}')
--- modules/optimization_funcs.py
+++ modules/optimization_funcs.py
@@ -1,4 +1,4 @@
-def PSO(X, same_class_mask, transformation,max_iter=1000):
+def PSO(X, same_class_mask, transformation,max_iter=1000, cpu_count=None):
     import pyswarms as ps
     import numpy as np
     from sklearn.metrics import pairwise_distances
@@ -26,5 +26,81 @@ def PSO(X, same_class_mask, transformation,max_iter=1000):
     optimizer = ps.single.GlobalBestPSO(n_particles= 10,
                                         dimensions = len(transformation),
                                         options=options)
-    _, A = optimizer.optimize(obj_func, max_iter, same_class_mask=same_class_mask,X_train=X,verbose=False)
+    _, A = optimizer.optimize(obj_func, max_iter, same_class_mask=same_class_mask,
+                              X_train=X, verbose=False, n_processes=cpu_count)
     return A
+
+
+def Gradient(X, same_class_mask, transformation,max_iter=1000,cpu_count=None):
+    import numpy as np
+    from sklearn.metrics import pairwise_distances
+    from sklearn.utils.extmath import softmax
+    #def alt_obj_func(x, same_class_mask, X_train):
+    #    from sklearn.metrics import pairwise_distances
+    #    from sklearn.utils.extmath import softmax
+    #    
+    #    X_embedded = np.dot(X_train,x.T)
+    #    p_ij = pairwise_distances(X_embedded, squared=True)
+    #    p_ij = softmax(-p_ij)
+    #    np.fill_diagonal(p_ij, 0.0)
+    #    
+    #    pi = same_class_mask * p_ij
+        
+    
+    def obj_func(x, same_class_mask, X_train):
+        from sklearn.metrics import pairwise_distances
+        from sklearn.utils.extmath import softmax
+        
+        for i in range(X_train.shape[0]):
+            
+            X_embedded = np.dot(X_train, x.T)
+            p_ij = pairwise_distances(X_embedded, squared=True)
+            p_ij = softmax(-p_ij)
+            np.fill_diagonal(p_ij, 0.0)
+            
+            p = 0.0
+            for j in range(X_train.shape[0]):
+                if same_class_mask[i][j]:
+                    p += p_ij[i][j]
+            
+            primeiro_termo = np.zeros( (x.shape[1],x.shape[1]) )
+            segundo_termo = np.zeros( (x.shape[1],x.shape[1]) )
+            
+            for k in range(X_train.shape[0]):
+                if i == k: continue
+                
+                xik = X_train[i] - X_train[k]
+                pik = p_ij[i][k]
+                term = pik * np.outer(xik,xik)
+                primeiro_termo += term
+                if same_class_mask[i][j]:
+                    segundo_termo += term
+            primeiro_termo *= p
+            
+            x += 0.5 * (primeiro_termo - segundo_termo)
+        return x
+    
+    def score(x, same_class_mask, X_train):
+        x = x.reshape(-1, X_train.shape[1])
+        X_embedded = np.dot(X_train, x.T)  # (n_samples, n_components)
+
+        # Compute softmax distances
+        p_ij = pairwise_distances(X_embedded, squared=True)
+        p_ij = softmax(-p_ij)  # (n_samples, n_samples)
+        np.fill_diagonal(p_ij, 0.0)
+        
+        masked_p_ij = p_ij * same_class_mask
+        p = np.sum(masked_p_ij, axis=1, keepdims=True)  
+        loss = np.sum(p)
+
+        return -1.0 * loss
+    
+    transformation = transformation.reshape(-1, X.shape[1])
+    for it in range(max_iter):
+        old_score = score(transformation, same_class_mask, X)
+        new_transformation = obj_func(transformation, same_class_mask, X)
+        new_score = score(new_transformation, same_class_mask, X)
+        if abs(old_score - new_score) < 0.001:
+            break
+
+    return np.ravel(transformation)
